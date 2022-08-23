8ea3a092066d80d8c21c6445dc597cd7db030fb4
Roberto de la Casa <rober.delacasa@syncrtc.com>
2022-08-23 16:52:30 +0200

    style(hy-button): Fix button styles

--- src/components/hy-button/hy-button.styles.js
+++ src/components/hy-button/hy-button.styles.js
@@ -2,33 +2,33 @@ import { css } from 'lit';
 
 export const HyButtonStyles = css`
   .hy-button {
-        font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-        font-weight: 700;
-        border: 0;
-        border-radius: 3em;
-        cursor: pointer;
-        display: inline-block;
-        line-height: 1;
-      }
-      .hy-button--primary {
-        color: white;
-        background-color: var(--sb-primary-color, #1ea7fd);
-      }
-      .hy-button--secondary {
-        color: #333;
-        background-color: transparent;
-        box-shadow: rgba(0, 0, 0, 0.15) 0 0 0 1px inset;
-      }
-      .hy-button--small {
-        font-size: 12px;
-        padding: 10px 16px;
-      }
-      .hy-button--medium {
-        font-size: 14px;
-        padding: 11px 20px;
-      }
-      .hy-button--large {
-        font-size: 16px;
-        padding: 12px 24px;
-      }
+    font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    border: 0;
+    border-radius: 3em;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 1;
+  }
+  .hy-button--primary {
+    color: white;
+    background-color:  #00C0B5;
+  }
+  .hy-button--secondary {
+    color: #333;
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0.15) 0 0 0 1px inset;
+  }
+  .hy-button--small {
+    font-size: 12px;
+    padding: 10px 16px;
+  }
+  .hy-button--medium {
+    font-size: 14px;
+    padding: 11px 20px;
+  }
+  .hy-button--large {
+    font-size: 16px;
+    padding: 12px 24px;
+  }
 `;
