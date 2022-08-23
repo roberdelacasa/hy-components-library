0b1e69e8be11422a42a17a16d63e036015e0d7f5
Roberto de la Casa <rober.delacasa@syncrtc.com>
2022-08-23 14:11:05 +0200

    chore(storybook): Add storybook config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,11 +1,11 @@
 module.exports = {
   stories: [
-    '../src/components/**/*.stories.mdx',
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials'
+    '@storybook/addon-essentials',
+    'storybook-dark-mode'
   ],
   framework: '@storybook/web-components',
   core: {
