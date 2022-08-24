5199f3cf4338c72d0bd3e7f241157aad41cb79de
Roberto de la Casa <rober.delacasa@syncrtc.com>
2022-08-24 13:21:49 +0200

    feat(hy-input): hy-input story

---
+++ src/components/hy-input/hy-input.stories.js
@@ -0,0 +1,38 @@
+import { html } from 'lit';
+import { userEvent, within, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { screen } from "shadow-dom-testing-library"
+
+import './hy-input';
+
+export default {
+  title: 'Example/Input',
+  component: 'hy-input',
+  parameters: {
+    actions: {
+      handles: ['key-pressed', 'enter-pressed', 'focus-lost'],
+    },
+  },
+}
+
+const Template = ({ value, placeHolder }) =>
+  html`<hy-input 
+        .value="${value}"
+        .placeHolder="${placeHolder}"
+        ></hy-input>`;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  value: '',
+  placeHolder: 'Search...'
+};
+
+
+Primary.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const input = await screen.getByShadowTestId('input');
+
+  await userEvent.type(input, 'example');
+  await waitFor(() => expect(input).toHaveValue('example'));
+};
